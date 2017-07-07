5a03f5d10b52d0083b13dc2fe1f5b723747c826a
Dariusz Jarosz <dariuszjarosz@me.com>
2017-07-07 14:56:04 -0500

    Add tools for easily building the angular application.

---
+++ sbin/build_ngApp_to_public.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+currentDir=`pwd`
+MY_DIR=`dirname $0`
+
+source $MY_DIR/configure_paths.sh
+
+path_to_npm=`which npm`
+
+if [ -z $path_to_npm ]; then
+    >&2 echo "ERROR: Could not find npm. Please run install_nodejs.sh and try again."
+    exit 1
+fi
+
+ngAppDir=$TASKINGTON_ROOT_DIR/ngApp
+
+cd $ngAppDir
+
+path_to_ng=`which ng`
+if [ -z $path_to_ng ]; then
+    npm install -g @angular/cli
+fi
+
+# Update any dependencies
+npm install
+
+ng build --target=production --base-href=/new-static/ --output-path=../new-public
+
+cd $currentDir
---
+++ sbin/configure_paths.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+MY_DIR=`dirname $0` && cd $MY_DIR && MY_DIR=`pwd`
+
+source $MY_DIR/../setup.sh
