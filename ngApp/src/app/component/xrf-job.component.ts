5c47c2f21211cd667dd2e44b68cfba449a668812
Dariusz Jarosz <dariuszjarosz@me.com>
2017-08-04 10:45:57 -0500

    Remove the functionality for email and job path. Integrate with the new widgets. Update the import references.

--- ngApp/src/app/component/xrf-job.component.ts
+++ ngApp/src/app/component/xrf-job.component.ts
@@ -1,23 +1,18 @@
 /**
  * Created by djarosz on 7/21/17.
  */
-import {Component, ViewEncapsulation} from "@angular/core";
-import {Directory} from "../service/model/Directory";
-import {ProcessNode} from "../service/model/ProcessNode";
-import {SchedulerService} from "../service/Scheduler.service";
-import {TreeNode} from "primeng/components/common/treenode";
-import {MapsProcValues} from "../utilities/model/mapsProcValues";
-import {MapsUtil} from "../utilities/mapsUtil";
-import {Job} from "../service/model/Job";
+import {Component} from "@angular/core";
+import {ProcessNode, Job, MapsArgs} from "../service/model/models";
+import { EmailInputComponent } from "./widget/widgets"
+import {GrowlService, SchedulerService} from "../service/services";
+import {MapsProcValues} from "../utility/model/mapsProcValues";
+import {MapsUtil} from "../utility/mapsUtil";
 import {SelectItem, Message} from "primeng/primeng";
-import {MapsArgs} from "../service/model/jobArgs/MapsArgs";
-import {GrowlService} from "../service/Growl.service";
-import { EmailInputComponent } from "./widgets/email-input.component"
+import {JobPathOptionsItem} from "./widget/job-path-input.component";
 
 const XFM0: string = "production";
 const XFM0_DATA2: string = "verify";
 const CNM: string = "cnm";
-const DIR_STRUCTURE_DEPTH = 3;
 
 const DEFAULT_BEAMLINE: string = '2-ID-E';
 const DEFAULT_PRIORITY: number = 5;
@@ -50,10 +45,8 @@ const EXPERIMENT_OPTIONS = [{label: MAPSPY_EXPERIMENT_NAME, value: MAPSPY_EXPERI
 })
 
 export class XrfJobComponent {
-  displayDirectorySelectionDialog: boolean = false;
-  directories: Directory[];
-  directorySelectionTree: TreeNode[];
-  selectedDirectory: TreeNode;
+  jobPathOptionsItemList: JobPathOptionsItem[];
+
   analysisSelection: MapsProcValues[] = MapsUtil.getOptionsForForm();
   processNodesSelectItemList: SelectItem[];
 
@@ -71,6 +64,11 @@ export class XrfJobComponent {
 
 
   constructor(private schedulerService: SchedulerService, private growlService: GrowlService) {
+    this.jobPathOptionsItemList = [];
+    this.jobPathOptionsItemList.push({displayPrompt: 'xfm0', serviceRequest: XFM0});
+    this.jobPathOptionsItemList.push({displayPrompt: 'xfm0-data2', serviceRequest: XFM0_DATA2});
+    this.jobPathOptionsItemList.push({displayPrompt: 'CNM', serviceRequest: CNM});
+
     this.growlMessages = [];
     this.mapsJobArguments = new MapsArgs;
     this.mapsJob = new Job;
@@ -123,121 +121,8 @@ export class XrfJobComponent {
     });
   }
 
-  makeDirectorySelection() {
-    if (this.selectedDirectory != null) {
-      let jobPath = "";
-      let directory = <Directory>this.selectedDirectory.data;
-      if (directory.parent != "#") {
-        let parentPath = directory.parent;
-        if (parentPath.charAt(parentPath.length -1) != '/') {
-          parentPath += '/';
-        }
-        jobPath = parentPath;
-      }
-
-      jobPath += directory.text;
-      this.mapsJob.DataPath = jobPath;
-    }
-    this.displayDirectorySelectionDialog = false;
-
-  }
-
-  showXfm0DirectoryDialog() {
-    this.loadDirectoryDialog(XFM0);
-  }
-
-  showXfm0Data2DirectoryDialog() {
-    this.loadDirectoryDialog(XFM0_DATA2);
-  }
-
-  showCNMDirectoryDialog() {
-    this.loadDirectoryDialog(CNM);
-  }
-
-  private loadDirectoryDialog(jobPath: string) {
-    this.schedulerService.getDatasetDirsList(jobPath, DIR_STRUCTURE_DEPTH)
-      .subscribe(
-        directories => {
-          this.updateDirectories(directories);
-          this.displayDirectorySelectionDialog = true;
-        }
-      );
-  }
-
-  private updateDirectories(directories: Directory[]) {
-    this.directories = directories;
-    let parentNode = null;
-    this.directorySelectionTree = [];
-
-    let directoryNodeList = [];
-    let lastParentIndex = 0;
-    for (let directory of this.directories) {
-      let newNode = {
-        label: directory.text,
-        data: directory,
-        expandedIcon: "fa-folder-open",
-        collapsedIcon: "fa-folder",
-        selectable: true,
-        parent: null,
-        expanded: false,
-      };
-
-      if (directory.parent != "#") {
-        if (directoryNodeList.length > 0) {
-          parentNode = directoryNodeList[lastParentIndex];
-          if (this.getFullParentPath(parentNode) != directory.parent) {
-            parentNode = null;
-            for (var i = 0; i < directoryNodeList.length; i++) {
-              let curTreeNode = directoryNodeList[i];
-              let parentPath = this.getFullParentPath(curTreeNode);
-              if (parentPath == directory.parent) {
-                parentNode = curTreeNode;
-                lastParentIndex = i;
-                break;
-              }
-            }
-          }
-          if (parentNode != null) {
-            if (parentNode.children == null) {
-              parentNode.children = [];
-            }
-
-            newNode.parent = parentNode;
-            parentNode.children.push(newNode);
-          }
-
-
-        }
-      } else {
-        // Expand the first parent.
-        newNode.expanded = true;
-      }
-
-      if (parentNode == null) {
-        this.directorySelectionTree.push(newNode);
-      }
-      directoryNodeList.push(newNode);
-    }
-  }
-
-  private getFullParentPath(treeNode: TreeNode): string {
-    let result = "";
-    if (treeNode.parent != null) {
-      let parent = treeNode.parent;
-      if (parent.parent == null) {
-        // Top most parent has the full path in text value
-        result += parent.data.text;
-      } else {
-        result += treeNode.data.parent;
-      }
-      // The server varies the return string to contain the / sometimes.
-      if (result.charAt(result.length -1) != '/') {
-        result += '/';
-      }
-    }
-
-    result += treeNode.data.text;
-    return result;
+  dataPathChange(event) {
+    this.mapsJob.DataPath = event;
   }
 
   emailAddressChange(event) {
