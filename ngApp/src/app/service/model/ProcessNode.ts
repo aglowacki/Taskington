c132745303c3157e5b52d77a73289b9d4acfdb7f
Dariusz Jarosz <dariuszjarosz@me.com>
2017-08-10 10:07:06 -0500

    Update the constructors for process nodes and jobs.

--- ngApp/src/app/service/model/ProcessNode.ts
+++ ngApp/src/app/service/model/ProcessNode.ts
@@ -8,10 +8,27 @@ export class ProcessNode {
   SupportedSoftware: string[];
   SystemCpuPercent: number;
   Port: number;
+  Hostname: string;
   SystemSwapPercent: number;
   ProcessMemPercent: number;
   SystemMemPercent: number;
   Heartbeat: string;
   ProcessCpuPercent: number;
   Id: number;
+
+  constructor() {
+    this.Status = "";
+    this.ComputerName = "";
+    this.NumThreads = 0;
+    this.SupportedSoftware = [];
+    this.SystemCpuPercent = 0;
+    this.Port = 0;
+    this.Hostname = "";
+    this.SystemSwapPercent = 0;
+    this.ProcessMemPercent = 0;
+    this.SystemMemPercent = 0;
+    this.Heartbeat = "";
+    this.ProcessCpuPercent = 0;
+    this.Id = 0;
+  }
 }
