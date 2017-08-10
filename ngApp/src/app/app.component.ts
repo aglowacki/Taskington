2acf9eb9cd795ca8a44fb212e2e3f0a3b463dfdb
Dariusz Jarosz <dariuszjarosz@me.com>
2017-08-10 10:09:20 -0500

    Update the process nodes table to allow row expandability. Add ptychography sumbission component.

--- ngApp/src/app/app.component.ts
+++ ngApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {SchedulerService, GrowlService} from "./service/services";
 import {ProcessNodes} from "./service/model/ProcessNodes";
 import {Observable} from "rxjs";
 import {Jobs} from "./service/model/Jobs";
+import {ProcessNode} from "./service/model/ProcessNode";
 
 @Component({
   selector: 'app-root',
@@ -22,8 +23,12 @@ export class AppComponent {
   processingJobs: Jobs;
   finishedJobs: Jobs;
 
+  private _nodeKeys: string[];
+  expandedNodes: ProcessNode[];
+
   constructor(private schedulerService: SchedulerService, public growlService: GrowlService) {
     this.loadData();
+    this.expandedNodes = [];
 
     Observable.interval(4000).subscribe(() => {
       this.loadData();
@@ -53,7 +58,25 @@ export class AppComponent {
 
   private loadProcessNodes() {
     this.schedulerService.getProcessNodes().subscribe(
-      processNodes => this.processNodes = processNodes
+      processNodes => {
+        this.processNodes = processNodes;
+        let newExpandedNodes: ProcessNode[] = [];
+        for (let expandedNode of this.expandedNodes) {
+          for (let node of processNodes.data) {
+            if (expandedNode.Id == node.Id) {
+              newExpandedNodes.push(node);
+            }
+          }
+        }
+        this.expandedNodes = newExpandedNodes;
+      }
     );
+  }
+
+  get nodeKeys(): string[] {
+    if (this._nodeKeys == null) {
+      this._nodeKeys = Object.keys(new ProcessNode());
+    }
+    return this._nodeKeys;
   }
 }
