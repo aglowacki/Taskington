dbf44d77a9b376f5a4e645568955814ae5a6b1c4
User 2 ide <User2ide@xor.aps.anl.gov>
2022-09-19 12:04:54 -0500

    update to xrf_module to fix filename off by 1. Set detector elements to 8

--- modules/xrf_maps.py
+++ modules/xrf_maps.py
@@ -31,7 +31,7 @@ def gen_args_dict():
 	return {
 		JOB_IS_LIVE_JOB:0,
 		JOB_STANDARDS:'maps_standardinfo.txt',
-		JOB_DETECTOR_ELEMENTS:4,
+		JOB_DETECTOR_ELEMENTS:8,
 		JOB_MAX_FILES_TO_PROC:1,
 		JOB_MAX_LINES_TO_PROC:-1,
 		JOB_QUICK_AND_DIRTY:0,
@@ -108,9 +108,9 @@ def gen_email_attachments(alias_path, job_dict):
 
 def start_job(log_name, alias_path, job_dict, options, exitcode):
 	job_args = job_dict[Constants.JOB_ARGS]
-	if job_args[JOB_IS_LIVE_JOB] == 1:
-		dataset_full_file_path = max(glob.iglob(alias_path + '/mda/*.mda'), key=os.path.getctime)
-		job_dict[Constants.JOB_DATASET_FILES_TO_PROC] = os.path.basename(dataset_full_file_path)
+	#if job_args[JOB_IS_LIVE_JOB] == 1:
+	#	dataset_full_file_path = max(glob.iglob(alias_path + '/mda/*.mda'), key=os.path.getctime)
+	#	job_dict[Constants.JOB_DATASET_FILES_TO_PROC] = os.path.basename(dataset_full_file_path)
 	try:
 		xrf_maps_path = options['Path']
 		xrf_maps_exe = options['Exe']
@@ -139,10 +139,10 @@ def start_job(log_name, alias_path, job_dict, options, exitcode):
 		detector_amount = int( str(job_args[JOB_DETECTOR_ELEMENTS]).strip() )
 		detector_end = detector_start + (detector_amount -1)
 
-		if detector_start < 0 or detector_start > 3: # we only have 4 detectors
+		if detector_start < 0:
 			detector_start = 0
-		if detector_end < detector_start or detector_end > 3: # we only have 4 detectors
-			detector_end = 3
+		if detector_end < detector_start:
+			detector_end = detector_start + 1
 
 		str_detector_range = str(detector_start) + ':' + str(detector_end)
 		args += ['--detector-range', str_detector_range]
@@ -154,12 +154,12 @@ def start_job(log_name, alias_path, job_dict, options, exitcode):
 		proc_mask = int(job_args[JOB_PROC_MASK])
 		key_d = 0
 		key_f = 0 # for netcdf to hdf5 future feature
+		if proc_mask & 4 == 4:
+			args += ['--fit'] + ['roi,nnls,matrix']
 		if proc_mask & 1 == 1:
 			args += ['--fit'] + ['roi,nnls']
 		if proc_mask & 2 == 2:
 			args += ['--optimize-fit-override-params']
-		if proc_mask & 4 == 4:
-			args += ['--fit'] + ['roi,nnls,matrix']
 		if proc_mask & 8 == 8:
 			key_d = 1
 		if proc_mask & 16 == 16:
